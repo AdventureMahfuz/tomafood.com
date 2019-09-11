a58818973d81bb0a4b0b715ba511fb47666bcd54
AdventureMahfuz <adventure.mahfuz@gmail.com>
2019-09-11 20:31:04 +0600

    fixed some issue

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -80,45 +80,25 @@
         });
 
 
-        //hero slider
-        /*$('#slider').owlCarousel({
-            animateIn: 'fadeIn',
-            animateOut: 'fadeOut',
-            loop:true,
-            margin:10,
-            autoplay: true,
-            responsiveClass:true,
-            nav:false,
-            dots:true,
-            mouseDrag:false,
-            touchDrag: false,
-            responsive:{
-                0:{
-                    items:1,
-                    dots:true
-                },
-                600:{
-                    items:1,
-                    dots:true
-                },
-                1000:{
-                    items:1,
-                    dots:true,
-                    loop:true
-                }
-            }
-        });*/
-        //slider animation
-        /*$('#slider').on('translate.owl.carousel',function () {
-            $('.item-content h1, .item-content p, .hero-cont-btn a, .hero-more-link a').removeClass('animated fadeInUp').css('opacity','0');
-            //$('.hero-cont-btn a').removeClass('animated slideInLeft').css('opacity','0');
-            //$('.hero-more-link a').removeClass('animated slideInRight').css('opacity','0');
+        /*popular cuisines select box start*/
+        $('.mvp.popular-items h4').click(function () {
+            $('.p-i-l').css({
+                'display':'block',
+                'top':'0',
+            });
         });
-        $('#slider').on('translated.owl.carousel',function () {
-            $('.item-content h1, .item-content p, .hero-cont-btn a, .hero-more-link a').addClass('animated fadeInUp').css('opacity','0');
-            //$('.hero-cont-btn a').addClass('animated slideInLeft').css('opacity','1');
-            //$('.hero-more-link a').addClass('animated slideInRight').css('opacity','1');
-        });*/
+        $('.p-i-l ul li').click(function () {
+            $('.p-i-l').css({
+                'display':'none',
+                'top':'20px',
+            });
+        });
+        $('.p-i-l ul li').click(function () {
+            $('#p-cuisines').val($(this).attr('data'));
+            $('#cuisine-name').html($(this).attr('data'));
+        });
+        /*popular cuisines select box end*/
+
 
 
         //WoW js activation
